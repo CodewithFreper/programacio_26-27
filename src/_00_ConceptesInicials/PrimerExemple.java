322737d2ffd96fe8c8e72c261e2a88ca757d6772
Cristina Fresneda (Freper) <codewithfreper@gmail.com>
2026-09-16 11:27:27 +0200

    Primer exemple.

---
+++ src/_00_ConceptesInicials/PrimerExemple.java
@@ -0,0 +1,19 @@
+package _00_ConceptesInicials;
+
+public class PrimerExemple {
+    public static void main(String[] args) {
+// Dades del programa
+        String nom = "Anna";
+        int edat = 18;
+        double nota = 7.5;
+        boolean aprovat = true;
+// Fem una operació
+        int edatAnyQueVe = edat + 1;
+// Mostrem informació
+        System.out.println("Hola " + nom);
+        System.out.println("Tens " + edat + " anys.");
+        System.out.println("L'any que ve tindràs " + edatAnyQueVe + ".");
+        System.out.println("La teua nota és " + nota);
+        System.out.println("Has aprovat? " + aprovat);
+    }
+}
